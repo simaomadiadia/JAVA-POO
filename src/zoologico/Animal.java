37e611912291f6b700e503c344887b401d846c94
Simao madiadia <simaomadiadia@email.com>
2022-11-18 18:09:47 +0000

    Criacao dos atributos

---
+++ src/zoologico/Animal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zoologico;
+
+/**
+ *
+ * @author User
+ */
+public class Animal {
+
+    public String nome;
+    public String especie;
+    public int idade;
+}
