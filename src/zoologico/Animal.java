2eaa7b05ffa395e9841930aa44490faa80620911
Simao madiadia <simaomadiadia@email.com>
2022-11-18 18:23:26 +0000

    Criacao dos metodos acessorios

--- src/zoologico/Animal.java
+++ src/zoologico/Animal.java
@@ -13,4 +13,29 @@ public class Animal {
     private String nome;
     private String especie;
     private int idade;
+    
+   public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome=nome;
+	} 
+	
+	public String getEspecie() {
+		return this.especie;
+	}
+	
+	public void setEspecie(String especie) {
+		this.especie=especie;
+	}
+	
+	public int getIdade() {
+		return this.idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade=idade;
+	}
+	
 }
